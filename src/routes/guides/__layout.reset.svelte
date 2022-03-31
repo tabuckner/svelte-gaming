2b052d5b24a0671bf1005f5fd40263be8361b9dc
Taylor Buckner <taylora.buckner@gmail.com>
2022-03-30 19:52:03 -0500

    Layout Reset.

---
+++ src/routes/guides/__layout.reset.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+  import Title from "$lib/title.svelte";
+  import "../../styles/globals.css";
+</script>
+
+<header>
+  <nav>
+    <Title title="Ninja Gaming Guides" />
+    <div class="links">
+      <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/guides">Guides</a>
+    </div>
+  </nav>
+</header>
+<main>
+  <slot><!-- optional fallback --></slot>
+</main>
+<footer>
+  <p>Copyright 2022 Taylor Buckner</p>
+</footer>
+
+<style>
+  header {
+    text-align: center;
+    background: rgba(0, 0, 0, 0.1);
+  }
+
+  main {
+    max-width: 960px;
+    margin: 24px auto;
+  }
+
+  footer {
+    text-align: center;
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    max-width: 960px;
+    margin: 0 auto;
+  }
+
+  .links {
+    margin-left: auto;
+  }
+
+  a {
+    margin-left: 8px;
+  }
+</style>
