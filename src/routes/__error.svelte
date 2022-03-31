f54b36b75c8f3ba2b5da9ac34ebfc7323a521424
Taylor Buckner <taylora.buckner@gmail.com>
2022-03-30 20:21:23 -0500

    Custom error page.

---
+++ src/routes/__error.svelte
@@ -0,0 +1,26 @@
+<script context="module">
+  /** @type {import('@sveltejs/kit').ErrorLoad} */
+  export function load({ error }) {
+    return {
+      props: { message: error.message },
+    };
+  }
+</script>
+
+<script>
+  export let message;
+</script>
+
+
+<div class="error">
+  <h2>Oops</h2>
+  <p>{message}</p>
+  <a href="/guides">View All Guides</a>
+</div>
+
+<style>
+  .error {
+    text-align: center;
+    margin-bottom: 56px;
+  }
+</style>
